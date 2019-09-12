d886c1ae7d4aee688f5fafe1fa1d9d83bb2f0d63
restart <szz1298@126.com>
2019-09-12 15:59:31 +0800

    restart

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+
+def index(request):
+    # return HttpResponse("Hello Django!")
+    return render(request, 'index.html')
+
+
+def login_action(request):
+    if request.method == "POST":
+        username = request.POST.get("username", "")
+        password = request.POST.get('password', '')
+        if username == 'admin' and password == "admin123":
+            return HttpResponseRedirect("/event_manage/")
+        else:
+            return render(request, "index.html", {'error': "username or password error!"})
+    else:
+        return render(request, "index.html", {'error': "username or password error!"})
+
+
+def event_manage(request):
+    return render(request, "event_manage.html")
