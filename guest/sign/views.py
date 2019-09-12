59d5d2052704228cc65aca2cf384e35730f01893
restart <szz1298@126.com>
2019-09-12 17:23:22 +0800

    restart

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -14,6 +15,14 @@ def login_action(request):
         username = request.POST.get("username", "")
         password = request.POST.get('password', '')
         if username == 'admin' and password == "admin123":
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)  # 登录
+                request.session['user'] = username
+                # response.set_cookie('user', username, 3600)  # 添加浏览器cookie
+                response = HttpResponseRedirect("/event_manage/")
+                return response
+
             return HttpResponseRedirect("/event_manage/")
         else:
             return render(request, "index.html", {'error': "username or password error!"})
@@ -21,5 +30,8 @@ def login_action(request):
         return render(request, "index.html", {'error': "username or password error!"})
 
 
+@login_required
 def event_manage(request):
-    return render(request, "event_manage.html")
+    # username = request.COOKIES.get('user', '')  # 读取浏览器cookie
+    username = request.session.get('user', '')
+    return render(request, "event_manage.html", {"user": username})
